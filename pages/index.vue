c6e832f1174ca91fad32b2fd2fa942c9ee560828
Boe Reh <justboereh@gmail.com>
2023-12-30 07:34:26 -0600

    Update dependencies and remove layout files, did a lot of index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,141 @@
 <script setup lang="ts">
+import { useWindowSize, watchIgnorable } from '@vueuse/core'
+import { compile, morph } from 'svg-path-morph'
+// @ts-ignore-next-line
+import { Gradient } from 'smooth-gradient'
+import { Roller } from 'vue-roller'
+import 'vue-roller/dist/style.css'
+
+const gradient = new Gradient('#ff931e', '#ff1d25')
+const paths = [
+  'M25.4,-1.2C25.4,10.3,12.7,20.7,-0.8,20.7C-14.2,20.7,-28.4,10.3,-28.4,-1.2C-28.4,-12.7,-14.2,-25.4,-0.8,-25.4C12.7,-25.4,25.4,-12.7,25.4,-1.2Z',
+  'm 50,-50 c 0,11.5 13.5,100 0,100 -13.4,0 -100,11.5 -100,0 0,-11.5 -13.4,-100 0,-100 13.5,0 100,-11.5 100,0 z',
+]
+const values = [
+  {
+    first: 'Frontend',
+    second: 'Developer',
+  },
+  {
+    first: 'Backend',
+    second: 'Developer',
+  },
+  {
+    first: 'Software',
+    second: 'Developer',
+  },
+  {
+    first: 'Software',
+    second: 'Engineer',
+  },
+  {
+    first: 'Your',
+    second: 'Engineer',
+  },
+  {
+    first: 'Your',
+    second: 'Developer',
+  },
+]
+const { width, height } = useWindowSize()
+const level = ref(0)
+const canChangeLevel = ref(true)
+const RollerValues = reactive({
+  first: 'frontend',
+  second: 'developer',
+  index: 0,
+})
+
+function UpdateTextGradient() {
+  const el = document.querySelectorAll<HTMLDivElement>('.roller:nth-child(1) .roller-item')
+
+  el.forEach((item, index) => (item.style.color = gradient.getColorFromValue((index / el.length) * 100)))
+}
+
+function ChangeLevel(level: 1 | 2 | 3) {}
+
+onMounted(() => {
+  UpdateTextGradient()
+
+  setInterval(() => {
+    if (RollerValues.index === values.length - 1) RollerValues.index = 0
+    else RollerValues.index++
+
+    RollerValues.first = values[RollerValues.index].first
+    RollerValues.second = values[RollerValues.index].second
+
+    setTimeout(UpdateTextGradient, 100)
+  }, 2000)
+})
+
 useHead({
-	title: 'Boe Reh',
+  title: 'justboereh',
+  htmlAttrs: {
+    class: 'bg-black text-white',
+  },
 })
 </script>
 
 <template>
-	<div class="hero grid place-items-center">
-		<span>
-			<span class="text-5xl font-bold">
-				<span class="my-name"> Hello, I'm Boe Reh. </span>
-				<br />
-				A full-stack developer.
-			</span>
+  <div class="p-4 z-10 pointer-events-none select-none">
+    <div class="mx-auto max-w-5xl flex justify-between">
+      <svg-logo class="h-6 sm:h-8" />
 
-			<br />
+      <span class="flex <sm:flex-col <sm:gap-2 pointer-events-auto gap-16">
+        <button
+          class="cursor-pointer hover:underline underline-offset-4 text-right"
+          type="button"
+        >
+          About
+        </button>
 
-			<p class="text-2xl text-black/70">
-				I mainly develop in Nuxt/Vue, but I dive into Next, Astro, Svelte.
-			</p>
+        <button
+          class="cursor-pointer hover:underline underline-offset-4 text-right"
+          type="button"
+        >
+          Projects
+        </button>
 
-			<br />
+        <button
+          class="cursor-pointer hover:underline underline-offset-4 text-right"
+          type="button"
+        >
+          Contact
+        </button>
 
-			<div class="flex gap-2">
-				<nuxt-link to="/contact">
-					<b-button type="fill">Contact Me</b-button>
-				</nuxt-link>
+        <button
+          class="cursor-pointer hover:underline underline-offset-4 text-right"
+          type="button"
+        >
+          CV
+        </button>
+      </span>
+    </div>
+  </div>
 
-				<nuxt-link to="/works">
-					<b-button type="text">See my works</b-button>
-				</nuxt-link>
-			</div>
-		</span>
-	</div>
+  <div class="fixed top-0 left-0 right-0 bottom-0 flex flex-col gap-4 justify-center items-center pointer-events-none">
+    <h1 class="text-md sm:text-2xl md:text-4xl">Boe Reh</h1>
+
+    <div class="flex text-2xl sm:text-5xl md:text-7xl gap-2">
+      <Roller
+        char-set="alphabet"
+        :value="RollerValues.first"
+      />
+
+      <Roller
+        char-set="alphabet"
+        :value="RollerValues.second"
+      />
+    </div>
+  </div>
+
+  <svg
+    class="fixed top-0 left-0 right-0 bottom-0"
+    :view-box="`0 0 ${width} ${height}`"
+  >
+    <path
+      fill="#fff"
+      :d="`m 0 0 c 0 5 8 5 8 0 C 8 -5 0 -5 0 0`"
+    />
+  </svg>
 </template>
-
-<style scoped>
-.hero {
-	height: calc(100dvh - 7rem);
-}
-
-.my-name {
-	background: linear-gradient(90deg, #b85436, #db2424);
-	background-clip: text;
-	-webkit-background-clip: text;
-	-webkit-text-fill-color: transparent;
-}
-</style>
--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, type Preset } from 'unocss'
 import transformerDirectives from '@unocss/transformer-directives'
+import transformerVariantGroup from '@unocss/transformer-variant-group'
+import presetIcons from '@unocss/preset-icons'
 
 export default defineConfig({
-    transformers: [transformerDirectives()],
+  // transformers: [transformerDirectives(), transformerVariantGroup()],
+  // presets: [
+  //   presetIcons({
+  //     collections: {
+  //       fluent: () => import('@iconify-json/fluent/icons.json').then((i) => i.default as any),
+  //     },
+  //   }) as Preset<Object>,
+  // ],
 })
